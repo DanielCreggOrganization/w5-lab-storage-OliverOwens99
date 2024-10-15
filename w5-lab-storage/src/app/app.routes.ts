d84323e5880bafa0db0299497d47630940a23387
OliverOwens99 <122319453+OliverOwens99@users.noreply.github.com>
2024-10-15 14:39:54 +0000

    lab final commit

---
+++ w5-lab-storage/src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'movies',
+    loadComponent: () => import('./pages/movies/movies.page').then( m => m.MoviesPage)
+  },
+];
